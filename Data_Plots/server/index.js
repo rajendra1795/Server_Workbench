7d98a9d8342c716c0f430fa10ae4696fafa76034
rajendra1795 <rprasad@developerscode.com>
2022-11-15 18:15:50 +0530

    Server and CRUD Successfull

--- Data_Plots/server/index.js
+++ Data_Plots/server/index.js
@@ -6,10 +6,10 @@ const mysql = require("mysql2");
 
 
 const db = mysql.createPool({
-    host :"208.91.199.11", 
-    user :"ztravilr_reuser",
-    password : "!yzN4a97",
-    database :"ztravilr_realestatecrm"
+    host :"localhost", 
+    user :"root",
+    password : "root123",
+    database :"employee_data"
 });
 
 app.use(cors());
@@ -18,71 +18,68 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 // Listen to API
 
-app.listen(5000, () => {
-    console.log(`Heloo World!...  Server is Running on Port: 5000`);
+app.listen(5001, () => {
+    console.log(`Heloo World!...  Server is Running on Port: 5001`);
 }); 
 
 // GET API (SELECT)
 
+
 app.get("/api/get", (req,res) => {
-    const sqlGet = "SELECT * FROM Dummy;";
+    const sqlGet = "SELECT * FROM employee;";
     db.query(sqlGet, (error, result) => {
         res.send(result);
     });
 });
 
-app.get("/api/get/Designations/", (req,res) => {
-    const sqlGet = "SELECT * FROM Designations;";
-    db.query(sqlGet, (error, result) => {
-        res.send(result);
-    });
-});
+
+// Sample Data Insertion 
+
+/*app.get("/", (req,res) => {
+    const sqlSampleInsert =
+         "INSERT INTO employee (emp_name, department, city, salary) VALUES ('Rajesh','Supervisior','Hyderabad', 16000)";
+    db.query(sqlSampleInsert,(error, result) => {
+        console.log("error", error);
+        console.log("result", result);
+       })
+       console.log("Data Inserted Successfully Go to /api/get");
+    })*/
 
 // POST API (INSERT)
 
 app.post("/api/post", (req,res) => {
-    const { DummyId,Name,Plots,Status } = req.body;
+    const { emp_name, department, city, salary } = req.body;
     const sqlInsert =
-         "INSERT INTO Dummy (DummyId, Name, Plots, Status) VALUES (?, ?, ?, ?)";
-    db.query(sqlInsert, [DummyId, Name, Plots, Status], (error, result) => {
+         "INSERT INTO employee (emp_name, department, city, salary) VALUES (?, ?, ?, ?)";
+    db.query(sqlInsert, [emp_name, department, city, salary], (error, result) => {
        if (error) {
         console.log(error);
        } 
     });
 });
 
-// PUT API (UPDATE)
 
-app.put("/api/update/:id", (req,res) => {
-    const { id } = req.params;
-    const { DummyId, Name, Plots, Status } = req.body;
-    const sqlUpdate = "UPDATE employee SET DummyId = ?, Name = ?, Plots = ?, Status = ? WHERE id = ?";
-    db.query(sqlUpdate, [DummyId,Name, Plots, Status, id], (error, result) => {
-        if (error) {
-            console.log(error);
-        }
-        res.send(result);
-    });
-});
+
 
 // DELETE API (BASED ON ID)
 
 app.delete("/api/delete/:id", (req,res) => {
     const { id } = req.params;
     const sqlDelete =
-         "DELETE FROM Dummy WHERE DummyId = ?";
+         "DELETE FROM employee WHERE id = ?";
     db.query(sqlDelete, id, (error, result) => {
        if (error) {
         console.log(error);
        } 
     });
 });
+
  
 // GET API ON SELECTED ID
  
 app.get("/api/view/:id", (req,res) => {
     const {id} = req.params;
-    const sqlGet = "SELECT * FROM Dummy WHERE id = ?";
+    const sqlGet = "SELECT * FROM employee WHERE id = ?";
     db.query(sqlGet, id, (error, result) => {
         if (error) {
             console.log(error);
@@ -91,9 +88,35 @@ app.get("/api/view/:id", (req,res) => {
     });
 });
 
+// GET API ON ID
+
+app.get("/api/get/:id", (req,res) => {
+    const { id } = req.params;
+    const sqlGet = "SELECT * FROM employee WHERE id = ?";
+    db.query(sqlGet, id, (error, result) => {
+       if (error) {
+        console.log(error);
+       } 
+       res.send(result);
+    });
+});
+
+// PUT API (UPDATE)
+
+app.put("/api/update/:id", (req,res) => {
+    const { id } = req.params;
+    const { emp_name, department, city, salary } = req.body;
+    const sqlUpdate = "UPDATE employee SET emp_name = ?, department = ?, city = ?, salary = ? WHERE id = ?";
+    db.query(sqlUpdate, [emp_name, department, city, salary, id], (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+        res.send(result);
+    });
+});
 
 
-//INSERT VALUES API 
+//INSERT VALUES DIFFERENT API'S
 
 /*app.get("/", (req,res)=> {
     const sqlInsert = "INSERT INTO AccountType (AccountTypeId, AccountName) VALUES (5, 'Savings')";
@@ -104,11 +127,11 @@ app.get("/api/view/:id", (req,res) => {
     });
 });*/
 
-app.get("/", (req,res)=> {
+/*app.get("/", (req,res)=> {
     const sqlInsert = "INSERT INTO Dummy (DummyId, Name, Plots, Status) VALUES (17, 'Korukonda', 175, 1)";
     db.query(sqlInsert, (error,result) => {
         console.log("error", error);
         console.log("result", result);
         res.send("Data Inserted")
     });
-});
+});*/
